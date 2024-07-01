c42525fabdcfcf1ef9a155a72671a8dfcb92a0ae
Kimi <82556573+Josegorn@users.noreply.github.com.>
2024-07-01 02:18:33 +0200

    0

--- js/inicio_portada.js
+++ js/inicio_portada.js
@@ -1,6 +1,5 @@
 "use strict";
 // 🔴 Inicialización
-//import * as DZ from "/js/diccionario.js";
 import { PAG_INDEX } from "/contenido/def/esquema.js";
 import { insertar_texto } from "./inicio_frame.js";
 
@@ -15,7 +14,7 @@ const RUTA_HTML = "/contenido/index.html";
 
 // 🔴 Inicialización
 document.addEventListener("DOMContentLoaded", function() {
-
+	
 	const RUTA_XML = PAG_INDEX[IDP].ruta+PAG_INDEX[IDP].archivo;
 	const TITULO_PAGINA = PAG_INDEX[IDP].titulo;
 	const TITULO = PAG_INDEX.descripcion;
