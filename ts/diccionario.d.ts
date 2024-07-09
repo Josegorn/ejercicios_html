b88ea1c9136750f6163f8f2a2de2c6692ad4770e
Kimi <82556573+Josegorn@users.noreply.github.com.>
2024-07-09 03:49:22 +0200

    0

--- ts/diccionario.d.ts
+++ ts/diccionario.d.ts
@@ -3,69 +3,34 @@ export class TipoObjeto {
     private o : symbol; 
     
     constructor() : symbol {
-        return this.o;
+        if(this.o === undefined) return this.o;
+        return undefined;
     }
     constructor(mnemonico : string) : boolean {
-        this.o = Symbol().for(mnemonico) ? true : false ;
-
+        this.o =  Symbol().for(mnemonico) ? true : false ;
     }
     public get cadena() {
-        return Symbol.keyFor(this.o);
+        return Symbol().keyFor(this.o);
     }
-};
-export type Entrada<const T = string | number | boolean | symbol > = {
-
-    private #_v : T  ;
-    
-    set (mnemonico : string) {
-        this.#_o = Symbol.for(mnemonico);
-        return this.#_o;
-    
-    public get () {
-        return this.
-};
-export type Entrada<const T = string | number | boolean | symbol > = {
-  
-    private #_v : T  ;
-       
-
-
-
-}     
-
-    
-
-    
-    
-
-    
-
-
-
-    
-      
-    public {
-         <string> = () => {
-            return this.#_v;
-        }
-    };
-    
 }
+export class Entrada<const T = string | number | boolean | symbol > {
 
-
-
-
-
-
-export type TipoEntrada = {
-    [key in TipoBoton]: {
-        [key in string]: Entrada<string>
-        
-    }         
+    private v : T ;
     
+    constructor() : T {
+        if (this.v === undefined) return this.v;
+        return undefined;  
+    }
+    constructor(valor : T) {
+        this.v = valor;
+    }
+    constructor(valor : symbol) : void {
+        this.v =  Symbol().for(valor) ? true : false ; 
+    }  
+    public get simbolo() {
+        return Symbol().keyFor(this.v);
+    }
+    public get tipo() {
+        return typeof(this.v);
+    } 
 }
-
-export default { TipoBoton, TipoEntrada };
-
-
-
--- ts/diccionario.ts
+++ ts/diccionario.ts
@@ -1,6 +1,41 @@
 "use strict";
 import  datos_diccionario from "/js/diccionario.json";
-import type  TipoEntrada from  "/ts/diccionario.d.ts";
+import type Entrada from  "/ts/diccionario.d.ts";
 
 
-export const DZ : WeakMap<WeakKey,TipoEntrada>(datos_diccionario);
+export class DZ WeakMap<WeakKey,Entrada>(datos_diccionario){
+    constructor(){
+        super(datos_diccionario);
+    }
+    public get(clave : string) : Entrada {
+        return super.get(clave);
+    }
+    public set(clave : string, valor : Entrada) : void {
+        return super.set(clave,valor);
+    }
+    public has(clave : string) : boolean {
+        return super.has(clave);
+    }
+    public delete(clave : string) : boolean {
+        return super.delete(clave);
+    }
+    public clear() : void {
+        return super.clear();
+    }
+    public size() : number {
+        return super.size;
+    }
+    public keys() : IterableIterator<string> {
+        return super.keys();
+    }
+    public values() : IterableIterator<Entrada> {
+        return super.values();
+    }
+    public entries() : IterableIterator<[string,Entrada]> {
+        return super.entries();
+    }
+    public forEach(callbackfn : (value : Entrada, key : string, map : WeakMap<WeakKey,Entrada>) => void, thisArg? : any) : void {
+        return super.forEach(callbackfn,thisArg);
+    }
+    
+}
