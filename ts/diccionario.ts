453ec114e275b2bae2f2de0d14d3ce6ec25a43d1
Kimi <82556573+Josegorn@users.noreply.github.com.>
2024-07-09 10:06:23 +0200

    0

--- ts/diccionario.ts
+++ ts/diccionario.ts
@@ -1,9 +1,5 @@
 "use strict";
-import { crearNodoTexto } from "./comun";
 import * as DZ from "/js/diccionario.json";
 
-JSON.parse(DZ).forEach(([key, value]) => {
-    (globalThis as any)[key] = value;
-});
-Object.entries(DZ).forEach(([key, value]) => {
-globalThis.[key] =  value;
+Object.entries(DZ).forEach(([key, value]) => {(globalThis as any)[key] = value as string});
+
